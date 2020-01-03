e8bd0a7736484f71b11f4219820a1080d2417267
Sajjan <s4jj4n@gmail.com>
2020-01-03 15:07:26 +0545

    ongoing

---
+++ web/app/themes/sajtheme/functions.php
@@ -0,0 +1,29 @@
+<?php
+
+//Adding Theme Support #
+//https://developer.wordpress.org/themes/functionality/post-formats/
+function themename_post_formats_setup()
+{
+    add_theme_support('post-formats', array('aside', 'gallery'));
+}
+
+add_action('after_setup_theme', 'themename_post_formats_setup');
+
+//To add an administration menu
+//https://developer.wordpress.org/themes/functionality/administration-menus/
+add_action('admin_menu', function () {
+    add_options_page(
+        'My Options',
+        'My menu haha',
+        'manage_options',
+        'my-unique-identifier',
+        function () {
+            if (!current_user_can('manage_options')) {
+                wp_die(__('You do not have sufficient permissions to access this page.'));
+            }
+            echo 'Here is where I output the HTML for my screen.';
+            echo '</div><pre>';
+        }
+    );
+});
+
