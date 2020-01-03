c2497196c90bc30441995e26248df8199f7caf2d
Sajjan <s4jj4n@gmail.com>
2020-01-03 17:39:55 +0545

    ongoing

--- web/app/themes/sajtheme/functions.php
+++ web/app/themes/sajtheme/functions.php
@@ -51,3 +51,22 @@ function themename_custom_header_setup() {
     add_theme_support( 'custom-header', $args );
 }
 add_action( 'after_setup_theme', 'themename_custom_header_setup' );
+
+
+
+add_action( 'widgets_init', 'my_register_sidebars' );
+function my_register_sidebars() {
+    /* Register the 'primary' sidebar. */
+    register_sidebar(
+        array(
+            'id'            => 'primary',
+            'name'          => __( 'Primary Sidebar' ),
+            'description'   => __( 'A short description of the sidebar.' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+    /* Repeat register_sidebar() code for additional sidebars. */
+}
--- web/app/themes/sajtheme/index.php
+++ web/app/themes/sajtheme/index.php
@@ -1,2 +1,12 @@
 
-<img src="<?php echo get_custom_header()->url ?>"/>
+<!--<img src="--><?php //echo get_custom_header()->url ?><!--"/>-->
+<?php if ( get_header_image() ) : ?>
+    <div id="site-header">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+        </a>
+    </div>
+<?php endif; ?>
+
+
+<?php get_sidebar( 'primary' ); ?>
